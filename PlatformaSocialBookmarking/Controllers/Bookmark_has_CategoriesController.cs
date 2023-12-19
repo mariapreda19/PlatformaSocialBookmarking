15b431d73eaa4486236c86b5d61a643b28c1f853
Tudor <miutudorgabriel@gmail.com>
2023-12-19 16:19:44 +0200

    save functionality

---
+++ PlatformaSocialBookmarking/Controllers/Bookmark_has_CategoriesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PlatformaSocialBookmarking.Data;
+using PlatformaSocialBookmarking.Models;
+
+public class Bookmark_Has_CategoriesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public Bookmark_Has_CategoriesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult SelectCategory(int id)
+    {
+        // Retrieve the bookmark by id
+        var bookmark = _context.Bookmarks.FirstOrDefault(b => b.Id == id);
+
+        if (bookmark == null)
+        {
+            // Handle the case where the bookmark is not found
+            return NotFound();
+        }
+
+        // Retrieve the list of categories
+        var categories = _context.Categories.ToList();
+
+        // Set up ViewBag for the view
+        ViewBag.BookmarkId = bookmark.Id;
+        ViewBag.Categories = new SelectList(categories, "Id", "CategoryName");
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Save(int bookmarkId, int selectedCategory)
+    {
+        // Check if the relationship already exists
+        if (!_context.Bookmark_Has_Categories.Any(bhc => bhc.BookmarkId == bookmarkId && bhc.CategoryId == selectedCategory))
+        {
+            // If not, create the relationship
+            var bookmarkHasCategory = new Bookmark_Has_Category
+            {
+                BookmarkId = bookmarkId,
+                CategoryId = selectedCategory
+            };
+
+            _context.Bookmark_Has_Categories.Add(bookmarkHasCategory);
+            _context.SaveChanges();
+        }
+        // If the relationship already exists, you might want to handle it accordingly (show a message, etc.)
+
+        // Redirect to the home page or display a success message
+        return RedirectToAction("Index", "Bookmarks");
+    }
+}
