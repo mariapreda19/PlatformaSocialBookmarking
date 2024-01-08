3f7ae7b8452d5d227debea15ecebf34f24dcaeb5
Tudor <miutudorgabriel@gmail.com>
2024-01-08 16:00:57 +0200

    alt nume roluri

--- PlatformaSocialBookmarking/Models/SeedData.cs
+++ PlatformaSocialBookmarking/Models/SeedData.cs
@@ -20,8 +20,7 @@ namespace PlatformaSocialBookmarking.Models
 
                 context.Roles.AddRange(
                     new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7210", Name = "Admin", NormalizedName = "Admin".ToUpper() },
-                    new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7211", Name = "Editor", NormalizedName = "Editor".ToUpper() },
-                    new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7212", Name = "User", NormalizedName = "User".ToUpper() }
+                    new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7211", Name = "UserInregistrat", NormalizedName = "UserInregistrat".ToUpper() }
                 );
 
                 var hasher = new PasswordHasher<ApplicationUser>();
@@ -46,16 +45,6 @@ namespace PlatformaSocialBookmarking.Models
                         Email = "userInregistrat@test.com",
                         NormalizedUserName = "USERINREGISTRAT@TEST.COM",
                         PasswordHash = hasher.HashPassword(null, "UserInregistrat1!")
-                    },
-                    new ApplicationUser
-                    {
-                        Id = "8e445865-a24d-4543-a6c6-9443d048cdb2", 
-                        UserName = "userNeinregistrat@test.com",
-                        EmailConfirmed = true,
-                        NormalizedEmail = "USERNEINREGISTRAT@TEST.COM",
-                        Email = "userNeinregistrat@test.com",
-                        NormalizedUserName = "USERNEINREGISTRAT@TEST.COM",
-                        PasswordHash = hasher.HashPassword(null, "UserNeinregistrat1!")
                     }
                 );
 
@@ -70,11 +59,6 @@ namespace PlatformaSocialBookmarking.Models
                     {
                         RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7211",
                         UserId = "8e445865-a24d-4543-a6c6-9443d048cdb1"
-                    },
-                    new IdentityUserRole<string>
-                    {
-                        RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7212",
-                        UserId = "8e445865-a24d-4543-a6c6-9443d048cdb2"
                     }
                 );
 
