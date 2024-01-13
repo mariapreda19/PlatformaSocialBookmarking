c95a8e211093df5f4113b558f5958c75f6229530
Tudor <miutudorgabriel@gmail.com>
2024-01-13 18:31:01 +0200

    baza de date 6

---
+++ PlatformaSocialBookmarking/Data/Migrations/20240113162939_migratie23.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlatformaSocialBookmarking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class migratie23 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
